7696e541e6520f574d4c59ec600a73a32d40edf7
Jady McIntyre <jadymcintyre@outlook.com>
2025-01-16 07:58:23 +0200

    functions backup

--- functions/functions/quit_net.zsh
+++ functions/functions/quit_net.zsh
@@ -1,6 +1,6 @@
 kn(){
 	# Search for the app and extract the PID
-	PID=$(kn | grep "/Applications/Netskope" | awk '{print $1}')
+	PID=$(ps -ax | grep "/Applications/Netskope" | awk '{print $1}')
 
 	# Check if PID is not empty
 	if [ -n "$PID" ]; then
