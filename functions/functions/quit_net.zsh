b4733b4ad2789edd1e38ebfbc74e14341beedd15
Jady McIntyre <jadymcintyre@outlook.com>
2025-01-16 07:59:13 +0200

    functions backup

--- functions/functions/quit_net.zsh
+++ functions/functions/quit_net.zsh
@@ -1,13 +1,12 @@
-kn(){
-	# Search for the app and extract the PID
-	PID=$(ps -ax | grep "/Applications/Netskope" | awk '{print $1}')
+kn() {
+    PID=$(ps -ax | grep "/Applications/Netskope" | grep -v "grep" | awk '{print $1}')
 
-	# Check if PID is not empty
-	if [ -n "$PID" ]; then
-		echo "Killing process with PID: $PID"
-		kill "$PID"
-		echo "Process terminated."
-	else
-		echo "No matching process found for Net"
-	fi
+    # Check if PID is not empty
+    if [ -n "$PID" ]; then
+        echo "Killing process with PID: $PID"
+        kill "$PID"
+        echo "Process terminated."
+    else
+        echo "No matching process found for /Applications/Netskope"
+    fi
 }
